04af47c1f47fa5e149e9f8e144d1534fe266ce49
francieleoliveira <franciele.oliveira@andritz.com>
2020-08-18 17:29:43 -0300

    challenge 02 project

---
+++ Module3/desafio-02/src/components/header/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {formatNumber} from '../../helpers/formatHelpers';
+import css from './header.module.css';
+
+export default class Header extends Component {
+
+  handleInputChange = (event) => {
+      const newText = event.target.value;
+      this.props.onChangeFilter(newText);
+  };
+
+  render() {
+    const {filter, countryCount, totalPopulation}= this.props;
+    return (
+      <div className={css.flexRow}>
+        <input 
+          type='text' 
+          value={filter} 
+          placeHolder="Filter"
+          onChange={this.handleInputChange}/> |
+        <span className={css.countries}>Países:<strong>{countryCount}</strong></span> |
+        <span className={css.population}>População:<strong> {formatNumber(totalPopulation)} </strong></span>
+      </div>
+    )
+  }
+}
